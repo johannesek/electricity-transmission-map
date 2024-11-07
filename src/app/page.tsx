ac379aa61501f708be64447bf1a3e394c78ec252
Johannes Ek <johannes.ek@aalto.fi>
2024-11-07 17:25:54 +0200

    additional info on utilization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
                     }
                     label="Animation speed"
                     minValue={0.001}
-                    maxValue = {0.2}
+                    maxValue = {0.5}
                     step={0.001}
                     value={step}
                     onChange={value => { changeStep(Number(value)); }}
@@ -243,7 +243,12 @@ export default function Home() {
                 <span className="inline-flex items-center gap-1">
                     <span>Utilization</span>
                     <span>
-                        <Tooltip content="Percentage of the maximum transmission obtained over the whole time period over the specific line.">
+                        <Tooltip content={
+                            <div className="px-1 py-2">
+                                <div className="text-small font-bold">Percentage of the maximum transmitted electricity over the whole time period over the specific line.</div>
+                                <div className="text-tiny">This might not reflect the utilization compared to the physical maximal capacity of the tansmission line.</div>
+                            </div>
+                        }>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                             </svg>
